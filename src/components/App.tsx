5532fe4df8d61ca6072a8093941899bc16f0aee2
nparshikov <nparshikov@baltbet.ru>
2024-08-28 14:10:17 +0300

    simple count

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const App = () => {
-  return <div> Hello Ass</div>;
+  const [count, setCount] = useState<number>(0);
+  const increment = () => setCount((prev) => prev + 1);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>inc</button>
+    </div>
+  );
 };
