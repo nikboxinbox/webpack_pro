0299893f7eb2f5ac4a61c7b97049a2ffd3546761
nparshikov <nparshikov@baltbet.ru>
2025-04-04 18:11:10 +0300

    base decomposition config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,8 @@
 import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { buildWebpack } from "./config/build/buildWebpack";
 
 
 interface EnvVariables {
@@ -14,56 +14,7 @@ export default (env: EnvVariables) => {
   const isDev = env.mode === "development";
   const isProd = env.mode === "production";
 
-  const config: webpack.Configuration = {
-    mode: env.mode ?? "development",
-    entry: path.resolve(__dirname, "src", "index.tsx"),
+  const config:webpack.Configuration =  buildWebpack();
 
-    output: {
-      path: path.resolve(__dirname, "build"),
-      filename: "[name].[contenthash].js",
-      clean: true,
-    },
-
-    plugins: [
-     isProd && new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "public", "index.html"),
-      }), new MiniCssExtractPlugin({
-         filename: "[name].[contenthash:8].css",
-         chunkFilename: "css/[name].[contenthash:8].css"
-      }),
-      isDev && new webpack.ProgressPlugin(),
-    ].filter(Boolean),
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            // mini-css-extract-plugin with the css-loader
-         isDev ? 'style-loader' :    MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
-          ],
-        },
-        {
-          test: /\.tsx?$/,
-          use: "ts-loader",
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-    },
-    devServer: isDev
-      ? {
-        port: env.port ?? 3000,
-        open: true,
-      }
-      : undefined,
-    devtool: isDev ? "inline-source-map" : false,
-  };
-  return config;
+  return config 
 };
